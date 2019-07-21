34d2c3c3389b67a23915aee90e05c5e9425231f8
peter <leepeter019@gmail.com>
2019-07-20 17:31:02 -0700

    added pawn class and methods to move pieces in board class

--- board.rb
+++ board.rb
@@ -1,5 +1,7 @@
 class Board
+
     attr_accessor :board
+
     def initialize
         @board = []
         8.times{ @board.push(Array.new(8, '_')) }
@@ -10,20 +12,33 @@ class Board
         (@board.length-1).downto(0) do |i|
             str += "#{i.to_s}|"
             @board.each_with_index do |item, idx|
-                str += "#{@board[idx][i].to_s}|"
+                (@board[idx][i].class==String) ? str += "#{@board[idx][i]}|" : str+="#{@board[idx][i].symbol}|"
             end
             str += "\n"
         end
         str += "  0 1 2 3 4 5 6 7 \n"
+
         print str
     end
 
-    def add_piece
-
+    def get(x, y)
+        @board[x][y]
     end
 
-end
+    def add(piece, location)
+        @board[location[0]][location[1]] = piece
+        piece.pos = location
+    end
 
-board = Board.new
-board.display
-puts board.board[6][5]
+    def remove(piece)
+        @board[piece.pos[0]][piece.pos[1]] = '_'
+        #add to graveyard
+    end
+
+    def move(piece, location)
+        add(piece, location)
+        remove(piece)
+    end
+
+
+end
---
+++ pawn.rb
@@ -0,0 +1,33 @@
+class Pawn
+
+    attr_accessor :pos
+    attr_reader :symbol, :player, :moves
+
+    def initialize(player)
+        @player = player
+        @pos = nil
+        @moves = []
+        @moved = false
+        @symbol = @player == 1 ? "\u{265F}" : "\u{2659}"
+    end
+
+    def get_moves(board)
+        moves = player == 1 ? [[0,1], [0,2], [1,1], [-1,1]] : [[0,-1], [0,-2], [1,-1], [-1,-1]]
+        
+        loc = []
+        moves.each { |item| loc.push([item[0] + @pos[0], item[1]+ @pos[1]]) }
+        
+        pieces = []
+        loc.each{ |item| pieces.push(board.get(item[0], item[1])) }
+        
+        if pieces[0] != nil && pieces[0].class == String
+            @moves.push(loc[0])
+            @moves.push(loc[1]) if pieces[1] != nil && pieces[1].class == String && !@moved
+        end
+
+        [pieces[2], pieces[3]].each_with_index { |item| @moves.push(item.pos) if item != nil && item.class != String }
+    
+    end
+
+
+end
